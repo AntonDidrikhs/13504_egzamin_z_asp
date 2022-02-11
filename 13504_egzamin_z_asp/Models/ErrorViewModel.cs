cd6ac326ff45447038dd910bbee0c3e6c1ef6f0d
AntonDidrikhs <didrikhsant@gmail.com>
2022-02-11 10:00:03 +0100

    Add project files.

---
+++ 13504_egzamin_z_asp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _13504_egzamin_z_asp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
